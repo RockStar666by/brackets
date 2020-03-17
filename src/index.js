59e5699112fb5c46ecf75b0f8c15155bbfcbbe04
RockStar666by <savchuk799@gmail.com>
2020-03-17 19:27:49 +0300

    add: sosution for brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let strTemp = '';
+    let strTemp2 = '';
+    let strJ = '';
+    for (let i = 0; i < str.length + 3; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            strJ = (bracketsConfig[j][0] + bracketsConfig[j][1]);
+            while (str.includes(strJ)) {
+                strTemp = str.replace(strJ, '');
+                strTemp2 = strTemp;
+                str = strTemp2;
+            }
+
+        }
+    }
+    if (str.length == 0) { return true } else { return false };
 }
